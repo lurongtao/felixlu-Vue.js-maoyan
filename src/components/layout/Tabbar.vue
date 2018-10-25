9de6872a4e220264ca8bf77a98f42e874f909e2c
lurongtao <lurongtao@126.com>
2018-10-25 18:56:35 +0800

    基础架子

---
+++ src/components/layout/Tabbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>tabbar</div>
+</template>
+<script>
+export default {
+  name: "Tabbar"
+};
+var x = 3;
+</script>
+<style lang="stylus" scoped>
+</style>
+
+
+
---
+++ src/pages/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header></Header>
+    <TabContainer></TabContainer>
+    <Tabbar></Tabbar>
+  </div>
+</template>
+<script>
+import Header from "./Header";
+import TabContainer from "./TabContainer";
+import Tabbar from "./Tabbar";
+export default {
+  name: "Home",
+  components: {
+    Header,
+    TabContainer,
+    Tabbar
+  }
+};
+</script>
+<style lang="stylus" scoped>
+</style>
+
